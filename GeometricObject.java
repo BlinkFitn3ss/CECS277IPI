358f15dccd9c6aec88acf645c17820f9dd0c51f7
BlinkFitn3ss <naveeneraya@gmail.com>
2019-09-23 15:53:01 -0700

    deleted geometricrunner added geometricobject

--- GeometricObject.java
+++ GeometricObject.java
@@ -5,8 +5,6 @@ package cecs277ipi.CECS277IPI;
  * @author Naveene Raya Carlos Alvarenga
  * @date 9/18/19
  */
-public class GeometricRunner {
-    //test GIT
-    //test #2
+public class GeometricObject {
     
 }

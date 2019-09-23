d21cbd48c7a118c1ddd69ee13b708e34c2f483c9
BlinkFitn3ss <Baby Wigglez@10.39.79.192>
2019-09-23 12:08:14 -0700

    some more fixes to style

--- GeometricRunner.java
+++ GeometricRunner.java
@@ -1,13 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package cecs277ipi.CECS277IPI;
 
 /**
  *
- * @author Baby Wigglez
+ * @author Naveene Raya Carlos Alvarenga
+ * @date 9/18/19
  */
 public class GeometricRunner {
     //test GIT

eb4ef9918eda7c2e7ef2079fd74e1f512349d6b5
Naveene Raya <48770741+BlinkFitn3ss@users.noreply.github.com>
2019-09-23 10:15:22 -0700

    Add files via upload

---
+++ GeometricObjectRunner.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cecs277hwgeometric;
+
+/**
+ *
+ * @author Naveene Raya Carlos Alvarenga
+ * @date 9/18/19 
+ */
+public class GeometricObjectRunner {
+    public static void main(String args[]){
+        
+    }
+}

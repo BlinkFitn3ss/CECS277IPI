38847b68affd63da23fa604da6288a4eb8c4db47
Carlos Alvarenga <50554392+a-eye-tech@users.noreply.github.com>
2019-09-25 01:16:24 -0700

    Update Circle.java
    Added definitions for constructor, Area(), Perimeter(), toString(), and added member variables many which are assigned values with the constructor. This code needs to be checked with an IDE for possible errors.

--- Circle.java
+++ Circle.java
@@ -12,6 +12,36 @@ public class Circle {
      *
      * @param centerPt
      */
-    public Circle(Point centerPt){
+    
+    final double PI = 3.14159;
+    
+    Point centerPt;
+    double radius;
+    
+    // the class has the following member variables which will be used in the toString() method.
+    double area;
+    double perimeter;
+    
+    // constructor calculates 'area' and'perimeter' and stores them in class member variables.
+    public Circle(Point centerPt_, double radius_){
+        centerPt = centerPt_;
+        radius = radius_;
+        area = PI*radius*radius;
+        perimeter = 2*PI*radius;
+    }
+    
+    @Override
+    public double Area(){
+        return area;
+    }
+    
+    @Override
+    public double Perimeter(){
+        return perimeter;
+    }
+    
+    @Override
+    public String toString(){
+        String str = "Circle: Center at:"+centerPt.toString()+" Radius: "+radius+" Area: "+area+" Perimeter: "+perimeter;
     }
 }

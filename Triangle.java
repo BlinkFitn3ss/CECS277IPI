6da87a963b9a0ba25570e02a9c8fe28f46d571bd
Carlos Alvarenga <50554392+a-eye-tech@users.noreply.github.com>
2019-09-24 23:57:50 -0700

    Update Triangle.java
    Added definitions for constructor, Area(), Perimeter(), toString(), and added member variables many which are assigned values with the constructor. This code needs to be checked with an IDE for possible errors.

--- Triangle.java
+++ Triangle.java
@@ -13,6 +13,45 @@ public class Triangle {
      * @param p2
      * @param p3
      */
-    public Triangle(Point p1, Point p2, Point p3){
+    Point p1;
+    Point p2;
+    Point p3;
+    
+    double sideA;
+    double sideB;
+    double sideC;
+    
+    double area;
+    double perimeter;
+    
+    public Triangle(Point p1_, Point p2_, Point p3_){
+        p1 = p1_;
+        p2 = p2_;
+        p3 = p3_;
+        sideA = p1.distance(p2);
+        sideB = p2.distance(p3);
+        sideC = p3.distance(p1);
+        
+        double s = (sideA+sideB+sideC)/2.0;
+        area = sqrt(s*(s-sideA)*(s-sideB)*(s-sideC));
+        perimeter = sideA+sideB+sideC;
+
     }
+    
+    @Override
+    public double Area(){
+        return area;
+    }
+    
+    @Override
+    public double Perimeter(){
+        return perimeter;
+    }
+    
+    @Override
+    public String toString(){
+        String str = "Triangle: "+p1.toString()+", "+p2.toString+", "+p3.toString+" Area: "+area+" Perimeter: "+perimeter;
+        return str;
+    }
+    
 }

76092d25bff3e9e0919d7ab57b8d392c816cddb5
Carlos Alvarenga <50554392+a-eye-tech@users.noreply.github.com>
2019-09-25 01:08:16 -0700

    Update Rectangle.java
    Added definitions for constructor, Area(), Perimeter(), toString(), and added member variables many which are assigned values with the constructor. This code needs to be checked with an IDE for possible errors.

--- Rectangle.java
+++ Rectangle.java
@@ -13,7 +13,34 @@ public class Rectangle {
      *
      * @param upperLeftPt
      */
-    public Rectangle(Point upperLeftPt){
+    
+    Point upperLeftPt;
+    double width;
+    double length;
+    
+    double area;
+    double perimeter;
+    
+    public Rectangle(Point upperLeftPt_, double width_, double length_){
+        upperLeftPt = upperLeftPt_;
+        width = width_;
+        length = length_;
+        area = width*length;
+        perimeter = 2*(width+length);
     }
     
+    @Override
+    public doubleArea(){
+        return area;
+    }
+    
+    @Override
+    public double Perimeter(){
+        return perimeter;
+    }
+    
+    @Override
+    public String toString(){
+        String str = "Rectangle: Upper left corner at: "+upperLeftPt.toString()+" ,"+" Width: "+width+" Length: "+length+" Area: "+area+" Perimeter: "+perimeter;
+    }
 }
